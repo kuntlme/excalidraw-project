c0e82ba51ac067952a0a6d760923137cafd4c18d
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-02-21 00:18:55 +0530

    simple auth done

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,13 +1,28 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on('connection', function connection(ws, url) {
+  const url = request.url; // ws://localhost:3000?token=123123
+
+  if(!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token =  queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if(!decoded || !(decoded as JwtPayload).userId){
+    ws.close();
+    return;
+  }
+
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    ws.send('pong');
   });
 
-  ws.send('something');
 });
