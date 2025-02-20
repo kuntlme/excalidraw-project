6bd1f3e4a4282255daa63830cb4d6c38760322ef
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-02-21 01:03:46 +0530

    common added for sharing types

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,42 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { Request, Response } from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {createUserSchema} from "@repo/common/types"
+
 const app = express();
+
+app.post("/signup", (req, res) => {
+
+    const {success} = createUserSchema.safeParse(req.body);
+    if(!success){
+        res.json({message: "Incorrect inputs"});
+        return;
+    }
+
+    res.json({
+        userId: 1234
+    })
+})
+
+app.post("/signin", (req, res) => {
+    const userId = 1;
+    jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        userId: 1234
+    })  
+})
+
+app.post("/room", middleware, (req: Request, res: Response) => {
+    res.json({
+        roomId: 123
+    })
+    return;
+})
 
 app.listen(3000, () => {
     console.log("listening the port");
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export const middleware = (req: Request, res: Response, next: NextFunction){
+    const token = req.headers["authorization"] ?? "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded){
+        res.status(403).json({message: "unauthorized"});
+    }
+    req.userId = token.userId;
+    next();
+}
